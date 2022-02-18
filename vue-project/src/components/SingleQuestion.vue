88a98687cee60741be3fe5333f78248d225b3be3
Vilgeir <vilgeir@gmail.com>
2022-02-18 14:57:43 +0100

    added question component and router-link

---
+++ vue-project/src/components/SingleQuestion.vue
@@ -0,0 +1,22 @@
+<style>
+  @import "./singleQuestion.css";
+</style>
+
+<script>
+</script>
+
+<template>
+    <div id="question-container">
+        <p>Hvor mange hull i en dorull?</p>
+        <div id="all-buttons">
+            <div>
+                <button class="answer">Answer 1</button>
+                <button class="answer">Answer 2</button>
+            </div>
+            <div>
+                <button class="answer">Answer 3</button>
+                <button class="answer">Answer 4</button>
+            </div>
+        </div>
+    </div>
+</template>
