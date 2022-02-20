2f622ac01f6c1929119ee768b4300395d7d18a12
Anne E. Larsen <anneelarsen98@gmail.com>
2022-02-20 13:42:10 +0100

    .

--- vue-project/src/views/Start.vue
+++ vue-project/src/views/Start.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="play-container">
-        <router-link to="/questions">   
+        <router-link to="/questions">
+            <select></select>
             <Button id="play-button">Play Now</Button>
         </router-link>
     </div>
