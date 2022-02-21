3b25dcb405dd4760c9d0881a3059495ff1d51fbf
Anne E. Larsen <anneelarsen98@gmail.com>
2022-02-21 15:32:15 +0100

    Users answer gets saved in store onclick.

--- vue-project/src/components/SingleQuestion.vue
+++ vue-project/src/components/SingleQuestion.vue
@@ -3,7 +3,8 @@
 </style>
 
 <script setup lang="ts">
-import type { PropType } from "vue";
+import { ref, type PropType, type Ref } from "vue";
+import { useStore } from "vuex";
 import type { Question } from "../api/questions";
 
 const props = defineProps({
@@ -13,23 +14,28 @@ const props = defineProps({
     }
 });
 
+const userAnswer:Ref<String> = ref("");
+const store = useStore();
+
+function funcClickQuestion(ans:string, que:Question) {
+    store.commit("setUserAnswer", [ans, que.question]);
+}
 </script>
 
-
 <template>
-<ul> 
+    <ul> 
         <div id="question-container" v-for="question in props.questions">
             <p>{{question.question}}</p>
             <div id="all-buttons">
                 <div id="tf-buttons">
-                    <button class="answer">{{question.correct_answer}}</button>
-                    <button class="answer">{{question.incorrect_answers[0]}}</button>
+                    <button class="answer" v-on:click="funcClickQuestion(question.correct_answer, question)" >{{question.correct_answer}}</button>
+                    <button class="answer" v-on:click="funcClickQuestion(question.incorrect_answers[0], question)">{{question.incorrect_answers[0]}}</button>
                 </div>
-                   <div v-if="question.type == 'multiple'">
-                    <button class="answer">{{question.incorrect_answers[1]}}</button>
-                    <button class="answer">{{question.incorrect_answers[2]}}</button>
+                <div v-if="question.type == 'multiple'">
+                    <button class="answer" v-on:click="funcClickQuestion(question.incorrect_answers[1], question)">{{question.incorrect_answers[1]}}</button>
+                    <button class="answer" v-on:click="funcClickQuestion(question.incorrect_answers[2], question)">{{question.incorrect_answers[2]}}</button>
                 </div>
             </div>
         </div>
-</ul>
+    </ul>
 </template>
